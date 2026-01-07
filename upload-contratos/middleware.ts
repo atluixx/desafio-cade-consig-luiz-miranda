9bd2517173d9d9ad1ef728916e178eeba239a270
atluixx <luizz.developer@gmail.com>
2026-01-07 10:48:08 +0100

    feat: route validation when user is authenticated

--- upload-contratos/middleware.ts
+++ upload-contratos/middleware.ts
@@ -3,13 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("access_token")?.value;
 
-  if (!token) {
+  if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (token && req.nextUrl.pathname === "/login") {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/login"],
 };
