bdd2bc6463e6c23d67ff4a87787f0c490fabac64
atluixx <luizz.developer@gmail.com>
2026-01-07 10:18:56 +0100

    chore: created /api/usuarios route

---
+++ upload-contratos/app/api/usuarios/route.ts
@@ -0,0 +1,74 @@
+import { NextRequest, NextResponse } from "next/server";
+import { api } from "..";
+import { z } from "zod";
+
+const usuarioSchema = z.object({
+  nome: z.string().min(1, "Nome é obrigatório"),
+  senha: z.string().min(1, "Senha é obrigatória"),
+});
+
+type Usuario = z.infer<typeof usuarioSchema>;
+
+export const GET = async (req: NextRequest) => {
+  const token = req.cookies.get("access_token")?.value;
+
+  if (!token) return NextResponse.json({ contratos: [], message: "Não autorizado", error: true }, { status: 401 });
+
+  const usuarios: Usuario[] = await api.get("/usuarios", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return NextResponse.json(
+    {
+      usuarios,
+      message: `${usuarios.length} contratos encontrados!`,
+      error: false,
+    },
+    { status: 200 },
+  );
+};
+
+export const POST = async (req: NextRequest) => {
+  const token = req.cookies.get("access_token")?.value;
+
+  if (!token) return NextResponse.json({ contrato: null, message: "Não autorizado", error: true }, { status: 401 });
+
+  let raw;
+
+  try {
+    raw = await req.json();
+  } catch {
+    return NextResponse.json({ contrato: null, message: "JSON inválido", error: true }, { status: 400 });
+  }
+
+  const parsed = usuarioSchema.safeParse(raw);
+
+  if (!parsed.success) {
+    const tree = z.treeifyError(parsed.error);
+
+    return NextResponse.json(
+      {
+        contrato: null,
+        message: "Dados inválidos",
+        errors: tree,
+        error: true,
+      },
+      { status: 400 },
+    );
+  }
+
+  const body: Usuario = parsed.data;
+
+  const usuario: Usuario = await api.post("/usuarios", body, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return NextResponse.json(
+    {
+      usuario,
+      message: "Usuário criado com sucesso!",
+      error: false,
+    },
+    { status: 200 },
+  );
+};
